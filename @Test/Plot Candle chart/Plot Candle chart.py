5dbf8d1d26f45792f01db5e1a6de2eb85ee6759e
guozc12 <guozc12@gmail.com>
2020-09-26 01:16:04 +0800

    try plot candle chart

---
+++ @Test/Plot Candle chart/Plot Candle chart.py
@@ -0,0 +1,16 @@
+"""This function is for plotting candle chart which imported from a CSV file"""
+
+import pandas as pd
+import mplfinance as mpf
+
+# read in data by pandas
+stock_data = pd.read_csv('sh600000.csv',
+                         parse_dates=['交易日期'])
+print(stock_data)
+
+# plot candle chart
+my_color = mpf.make_marketcolors(up='red', down='green')
+my_style = mpf.make_mpf_style(marketcolors=my_color)
+title='SH600000'
+stock_data.set_index('交易日期', inplace=True)
+mpf.plot(stock_data, type='candle', title=title, ylabel="price", style=my_style)
