480a2c7e466b27c403003a133f244cc9f7dea604
guozc12 <guozc12@gmail.com>
2020-09-26 15:34:53 +0800

    tiny modification

--- @Test/Plot Candle chart/Plot Candle chart.py
+++ @Test/Plot Candle chart/Plot Candle chart.py
@@ -11,6 +11,6 @@ print(stock_data)
 # plot candle chart
 my_color = mpf.make_marketcolors(up='red', down='green')
 my_style = mpf.make_mpf_style(marketcolors=my_color)
-title='SH600000'
+title = 'SH600000'
 stock_data.set_index('交易日期', inplace=True)
-mpf.plot(stock_data, type='candle', title=title, ylabel="price", style=my_style)
+mpf.plot(stock_data, type = 'candle', title = title, ylabel = "price", style = my_style)
